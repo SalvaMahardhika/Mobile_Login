5ac60513d162a75975166faf5bc49f348cb9821e
SalvaMahardhika <yoi198808@gmail.com>
2025-02-26 13:54:59 +0700

    logcat

--- app/src/main/java/com/salva/lojin/LoginActivity.kt
+++ app/src/main/java/com/salva/lojin/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.salva.lojin
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -29,6 +30,8 @@ class LoginActivity : AppCompatActivity() {
         loginButton.setOnClickListener {
             val email = emailEditText.text.toString()
             val password = passwordEditText.text.toString()
+
+            Log.d("LoginActivity", "Email yang dimasukkan: $email") // Menampilkan email di Logcat
 
             if (email.isNotEmpty() && password.isNotEmpty()) {
                 // Pindah ke DashboardActivity setelah login sukses
